b858157ac65bdaf974331cdf238b47a9dd14ab23
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-03-08 18:19:44 +0100

    ej2

---
+++ lab04/ejercicio2.py
@@ -0,0 +1,22 @@
+class Mochila:
+    
+    def __init__():
+        self.objetos = []
+    
+    def __str__(self):
+        for objeto in self.objetos:
+            print(objeto)
+
+    def introducir_objeto(a):
+        self.objetos.append(a)
+    
+    def main():
+        m2 = Mochila()
+        m3 = Mochila()
+        m2.introducir_objeto('bocadillo')
+        m2.introducir_objeto('juego de llaves')
+        print(m2)
+        print(m3)
+
+    if __name__== "__main__":
+        main()
