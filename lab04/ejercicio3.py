2d435bfbfce8d2facc1b6a17d162bda28955191b
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2023-03-13 18:56:08 +0100

    a

--- lab04/ejercicio3.py
+++ lab04/ejercicio3.py
@@ -10,7 +10,26 @@ class Diccionario:
 
     def cargar(self, fichero, origen):
         g = open(fichero, "r")
+
+        #si el origen es eu,clave palabra en euskera
         if(origen == "eu"):
-            
-          
-        
+            while line.readline():
+                lerroa = line.readline()
+                clave = lerroa.split[0]
+                valor = lerroa.split[1]
+                self.dictionary[clave] = valor
+
+        #si el origen es en, clave palabra en ingles
+        else: 
+            while line.readline():
+                lerroa = line.readline()
+                clave = lerroa.split[1]
+                valor = lerroa.split[0]
+                self.dictionary[clave] = valor
+        g.close()
+
+        def consultar(self, palabra):
+            if self.dictionary.has_key(palabra):
+                return self.dictionary.get(palabra)
+            else:
+                return " "
